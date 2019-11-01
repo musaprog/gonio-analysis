34709a989d7d1b22da8d40fd7ff556e7e353f515
Joni <jjtkemppainen1@sheffield.ac.uk>
2019-11-01 14:08:23 +0000

    Add bin folder for runnable scripts
    Former-commit-id: 7e33a713b640f781569400f93fba4da0b1fab419

---
+++ bin/adm_gui.cmd
@@ -0,0 +1,1 @@
+python adm_gui.py
---
+++ bin/tk_examine.py
@@ -0,0 +1,13 @@
+import os
+import sys
+# Add the directory above the pupil folder to path because
+# other needed packages may lay there
+sys.path.append(os.path.dirname(os.path.dirname(os.getcwd())))
+
+from pupil.drosom.gui.examine import main
+
+
+if __name__ == "__main__":
+    main()
+
+

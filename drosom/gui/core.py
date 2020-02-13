9e302cd97c30520127c37bb8adb2d523fdbdc280
Joni <jjtkemppainen1@sheffield.ac.uk>
2020-02-13 13:50:06 +0000

    Fix getting specimens with correction
    Former-commit-id: a2a904e1c41d36f1f94e10d178ea614e14c4e17f

--- drosom/gui/core.py
+++ drosom/gui/core.py
@@ -56,7 +56,7 @@ class Core:
             specimens = [specimen for specimen in specimens if self.get_manalyser(specimen, no_data_load=True).is_measured() == with_movements]
         
         if with_correction is not None:
-            specimens = [specimen for specimen in specimens if self.get_manalyser(specimen, no_data_load=True).get_antenna_level_correction() == with_correction]
+            specimens = [specimen for specimen in specimens if bool(self.get_manalyser(specimen, no_data_load=True).get_antenna_level_correction()) == with_correction]
         
         hidden = self.get_hidden()
         

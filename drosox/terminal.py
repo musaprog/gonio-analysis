d77463170d301b460cd90d13cb0e00ec7d00b032
Joni <jjtkemppainen1@sheffield.ac.uk>
2020-10-15 09:02:01 +0100

    Refactor drosox.py to it's own folder.
    Now DrosoX code (for analysing static DPP imaging data) lies in it's
    own package like DrosoM code does.
    
    Also improvements like command line interface with argparse module.
    
    
    Former-commit-id: 2247b0b9a32bf4199846a92e0129220a9a66505a

---
+++ drosox/terminal.py
@@ -0,0 +1,79 @@
+import os
+import argparse
+
+import matplotlib.pyplot as plt
+
+from pupil.directories import DROSO_DATADIRS
+from pupil.droso import DrosoSelect, simple_select
+from pupil.drosox.analysing import XAnalyser
+from pupil.drosox.plotting import (
+        plot_1d_overlap,
+        plot_matrix_overlap,
+        plot_experiment_illustration
+)
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='DrosoX: Analyse DPP static imaging data')
+    parser.add_argument('datadir',
+            help='Path to the specimens data directory or ASK')
+    
+    parser.add_argument('specimens',
+            help='Comma separated list of specimens or ASK')
+
+   
+    parser.add_argument('--measure-overlap',
+            help='(Re)measure binocular overlap using user-assisted binary search',
+            action='store_true')
+
+    # Plotting arguments
+    parser.add_argument('--plot-overlap',
+            help='Plot 1D binocular overlap',
+            action='store_true')
+    parser.add_argument('--plot-matrix-overlap',
+            help='Plot binocular overlap as discrete rectangulars',
+            action='store_true')
+    parser.add_argument('--plot-experiment-illustration',
+            help='Plot illustrative video how the experiments were done',
+            action='store_true')
+
+
+
+    args = parser.parse_args()
+    
+    if args.datadir.lower() == 'ask':
+        datadir = simple_select(DROSO_DATADIRS)
+    else:
+        datadir = args.datadir
+
+
+    if args.specimens.lower() == 'ask':
+        specimens = DrosoSelect(datadir=datadir).ask_user()
+        specimens = [os.path.basename(specimen) for specimen in specimens]
+    else:
+        specimens = args.specimens.split(',')
+    
+
+    xanalysers = [XAnalyser(datadir, specimen) for specimen in specimens]
+
+    # Perform the tasks that have to be performed per xanalyser
+    for xanalyser in xanalysers:
+        if args.measure_overlap:
+            xanalyser.measure_overlap()
+        
+        if args.plot_experiment_illustration:
+            plot_experiment_illustration(xanalyser)
+
+   
+    if args.plot_overlap:        
+        plot_1d_overlap(xanalysers)
+    elif args.plot_matrix_overlap:
+        plot_matrix_overlap(xanalysers)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+

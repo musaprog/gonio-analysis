dfecd612630808813c66b19ad993c7431df3481f
Joni Kemppainen <huniPC>
2020-11-17 23:16:21 +0000

    Separate parse_specimens from DrosoSelect
    Needed for new terminal changes, for non-interactive selection.

--- pupilanalysis/droso.py
+++ pupilanalysis/droso.py
@@ -70,6 +70,59 @@ class DrosoSelect:
         self.folders = [os.path.join(self.path, fn) for fn in folders]
  
         self.groups = SpecimenGroups()
+    
+
+    def parse_specimens(self, user_input):
+        '''
+        Parse user input to get manalyser names.
+
+        Arguments
+        ---------
+        *user_input : string
+            Comma separated list of specimen names or indices of self.folders,
+            or a group name.
+        
+        Raises ValueError if unable to parse.
+        '''
+        
+        # 1) If user supplies a specimen group name
+        if user_input in self.groups.get_groups().keys():
+            user_input = ','.join(self.groups.get_specimens(user_input))
+
+            sel_keys = [os.path.basename(x) for x in user_input.split(',')]
+            selections = [folder for folder in self.folders if os.path.basename(folder) in sel_keys]
+            
+            if len(selections) == len(sel_keys):
+                print('Selecting by group.')
+            else:
+                print('Group is invalid; Specified specimens do not exist')
+         
+        else:
+            # 2) Otherwise first try if indices to self.folders
+            try:
+                sel_indices = [int(i) for i in user_input.split(',')]
+                selections = [folders[i] for i in sel_indices]
+            except IndexError:
+                print('One of the given numbers goes over limits, try again.')
+            
+            # 3) Next try if specifying by specimen names
+            except ValueError:
+                print('Not number values given, trying with base names')
+                
+                sel_keys = [os.path.basename(x) for x in user_input.split(',')]
+                selections = [folder for folder in self.folders if os.path.basename(folder) in sel_keys]
+                
+                if len(selections) == len(sel_keys):
+                    print('Worked.')
+                else:
+                    print('Did not work, try again.')
+        
+        try:
+            selections
+        except:
+            ValueError("parse_specimens unable to process {}".format(user_input))
+
+        return selections
         
 
     def ask_user(self, startswith='', endswith='', contains=''):
@@ -109,41 +162,13 @@ class DrosoSelect:
                 self.groups.new_group(*splitted[1:])
             elif splitted[0] == 'list_groups':
                 print(self.groups.get_groups())
-            
-            elif user_input in self.groups.get_groups().keys():
-                user_input = ','.join(self.groups.get_specimens(user_input))
-
-                sel_keys = [os.path.basename(x) for x in user_input.split(',')]
-                selections = [folder for folder in self.folders if os.path.basename(folder) in sel_keys]
-                
-                if len(selections) == len(sel_keys):
-                    print('Selecting by group.')
-                    break
-                else:
-                    print('Group is invalid; Specified specimens do not exist')
-             
-
             else:
-
                 try:
-                    sel_indices = [int(i) for i in user_input.split(',')]
-                    selections = [folders[i] for i in sel_indices]
+                    selections = self.parse_specimens(user_input)
                     break
-                except IndexError:
-                    print('One of the given numbers goes over limits, try again.')
                 except ValueError:
-                    
-                    print('Not number values given, trying with base names')
-                    
-                    sel_keys = [os.path.basename(x) for x in user_input.split(',')]
-                    selections = [folder for folder in self.folders if os.path.basename(folder) in sel_keys]
-                    
-                    if len(selections) == len(sel_keys):
-                        print('Worked.')
-                        break
-                    else:
-                        print('Did not work, try again.')
-                 
+                    pass
+
         print('\nSelected {}\n'.format(selections))
         
         self.groups.save_groups()

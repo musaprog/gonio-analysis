358ba0ebe4573528148a601a7d81cbbfca056322
Joni Kemppainen <huniPC>
2022-01-08 16:22:42 +0200

    Rework kinematics analysis

--- gonioanalysis/drosom/kinematics.py
+++ gonioanalysis/drosom/kinematics.py
@@ -122,9 +122,9 @@ def latency(manalyser, image_folder, threshold=0.05, method='sigmoidal'):
     
     Returns
     -------
-    latency : float
-        The time duration in seconds that it takes for the response
-        to reach (by default) 5% of its maximum value
+    latency : sequency 
+        The time durations in seconds that it takes for the responses
+        to reach (by default) 5% of its maximum value (length of repeats).
     '''    
     fs = manalyser.get_imaging_frequency(image_folder)
     
@@ -151,20 +151,28 @@ def latency(manalyser, image_folder, threshold=0.05, method='sigmoidal'):
 
 
 
-def _sigmoidal_fit(displacements, fs):
+def _sigmoidal_fit(displacements, fs, debug=True):
     amplitudes = []
     speeds = []
     latencies = []
- 
+
+    if debug:
+        fig, ax = plt.subplots()
+
     timepoints = np.linspace(0, len(displacements[0])/fs, len(displacements[0]))
 
     for i_repeat, displacement in enumerate(displacements):
         
         # Initial guesses for k,L,t0
-        est_t0 = (np.argmax(displacement)/fs)/2
-        est_L = np.max(displacement)
-        est_k = est_L/est_t0
+        est_L = displacement[-1]
+        if est_L > 0:
+            est_t0 = (np.argmax(displacement)/fs)/2
+        else:
+            est_t0 = (np.argmin(displacement)/fs)/2
+        est_k = abs(est_L/est_t0)
         
+        print('est L={} t0={} k={}'.format(est_L, est_t0, est_k))
+
         try:
             popt, pcov = scipy.optimize.curve_fit(_logistic_function, timepoints, displacement,
                     p0=[est_k, est_L, est_t0])
@@ -173,30 +181,51 @@ def _sigmoidal_fit(displacements, fs):
             # Usually then we have nonsigmoidal data (no response)
             continue
        
+       
+        if debug:
+            ax.plot(timepoints, displacement, '-')
+            ax.plot(timepoints, _logistic_function(timepoints, *popt),
+                    '--', label='fit rep {}'.format(i_repeat))
+            
+            plt.show(block=False)
+            plt.pause(.1)
+            if not input('good?')[0].lower() == 'y':
+                plt.close(fig)
+                return None
+            plt.close(fig)
+
         speeds.append(popt[0])
         amplitudes.append(popt[1])
         latencies.append(popt[2])
+ 
 
     return amplitudes, speeds, latencies
 
 
 
-def sigmoidal_fit(manalyser, image_folder, figure_savefn=None, debug=False):
+def sigmoidal_fit(manalyser, image_folder, figure_savefn=None):
     '''
 
     Assuming sigmoidal (logistic function) response.
     
+    Arguments
+    ---------
+    manalyser : object
+    image_folder : string
+    figure_savefn : string
+        If given, saves a figure of the fit
+
     Returns the following lists
-        amplitudes, speeds, latencies
+        amplitudes, speeds, halfrise_times
     '''
 
-    if figure_savefn is not None or debug:
+    if figure_savefn is not None:
         fig, ax = plt.subplots()
 
 
     amplitudes = []
     speeds = []
-    latencies = []
+    halfrise_times = []
     
     pcovs = []
     
@@ -205,42 +234,30 @@ def sigmoidal_fit(manalyser, image_folder, figure_savefn=None, debug=False):
 
     timepoints = np.linspace(0, len(displacements[0])/fs, len(displacements[0]))
 
+    
     for i_repeat, displacement in enumerate(displacements):
         
-        # Initial guesses for k,L,t0
-        est_t0 = (np.argmax(displacement)/fs)/2
-        est_L = np.max(displacement)
-        est_k = est_L/est_t0
-        
-        try:
-            popt, pcov = scipy.optimize.curve_fit(_logistic_function, timepoints, displacement,
-                    p0=[est_k, est_L, est_t0])
-        except RuntimeError:
-            # Runtime Error occurs when curve fitting takes over maxfev iterations
-            # Usually then we have nonsigmoidal data (no response)
-            continue
+        amplitude, speed, halfrisetime = sigmoidal_fit([displacement], fs)
        
-        speeds.append(popt[0])
-        amplitudes.append(popt[1])
-        latencies.append(popt[2])
+        speeds.append(speed[0])
+        amplitudes.append(amplitude[0])
+        halfrise_times.append(halfrise_time[0])
 
-        if figure_savefn is not None or debug:
+        if figure_savefn is not None:
             ax.plot(timepoints, displacement, '-')
             ax.plot(timepoints, _logistic_function(timepoints, *popt),
                     '--', label='fit rep {}'.format(i_repeat))
     
 
-    if figure_savefn is not None:
+    if figure_savefn:
         fig.savefig(figure_savefn)
-    if debug:
-        plt.show()
-        
 
-    return amplitudes, speeds, latencies
+    return amplitudes, speeds, halfrise_times
 
 
 
-def save_sigmoidal_fit_CSV(analysers, savefn, save_fits=True):
+def save_sigmoidal_fit_CSV(analysers, savefn, save_fits=False, with_extra=True,
+        microns=True):
     '''
     Takes in analysers, performs sigmoidal_fit for each and all image_folders.
     Then saves the results as a CSV file, and by default fit images as well.
@@ -260,7 +277,24 @@ def save_sigmoidal_fit_CSV(analysers, savefn, save_fits=True):
         writer.writerow(['# Kinematics by sigmoidal fit', 'L / (1+np.exp(-k*(t-t0)))'])
         writer.writerow(['# t0 latency (s)', 'k rise speed (pixels/s)', 'L response amplitude (pixels)'])
         writer.writerow(['#'])
-        writer.writerow(['Name', 'Image folder',  'Mean L (pixels)', 'STD L (pixels)' 'Mean k (pixels/s)', 'STD k (pixels/s)', 'Mean t0 (s)', 'STD t0 (s)', 'Fit image'])
+
+        if microns:
+            y_units = 'pixels'
+        else:
+            y_units = 'µm'
+
+        header = ['Name', 'Image folder',
+                'Mean L ({})', 'STD L ({})',
+                'Mean k ({}/s)', 'STD k ({}/s)',
+                'Mean t0 (s)', 'STD t0 (s)', 'Fit image']
+
+        if with_extra:
+            header.extend( ['Mean max amplitude ({})',
+                'Mean final amplitude ({})' ,
+                'Mean latency (s)',
+                'Top speed ({}/s)'] )
+
+        writer.writerow([text.format(y_units) for text in header])
         
         i_fit = 0
 
@@ -278,14 +312,34 @@ def save_sigmoidal_fit_CSV(analysers, savefn, save_fits=True):
                 figure_savefn = False
 
             for image_folder in analyser.list_imagefolders():
-                amplitudes, speeds, latencies = sigmoidal_fit(analyser, image_folder,
+                
+                amplitudes, speeds, halfrise_times = sigmoidal_fit(analyser, image_folder,
                         figure_savefn=figure_savefn)
+                
+                if microns:
+                    scaler = analyser.get_pixel_size(image_folder)
+                else:
+                    scaler = 1
+                
+                if figure_savefn:
+                    figure_savefn = os.path.basename(figure_savefn)
 
-                writer.writerow([analyser.folder, image_folder,
-                    np.mean(amplitudes), np.std(amplitudes),
-                    np.mean(speeds), np.std(speeds),
-                    np.mean(latencies), np.std(latencies),
-                    os.path.basename(figure_savefn)])
+                row = [analyser.folder, image_folder,
+                        np.mean(amplitudes)*scaler, np.std(amplitudes)*scaler,
+                        np.mean(speeds)*scaler, np.std(speeds)*scaler,
+                        np.mean(halfrise_times), np.std(halfrise_times),
+                        figure_savefn]
+
+                if with_extra:
+                    max_amplitude = scaler * mean_max_response(analyser, image_folder, maxmethod='max')
+                    end_amplitude = scaler * mean_max_response(analyser, image_folder, maxmethod='final')
+                    latency_value = np.mean(latency(analyser, image_folder))
+                    
+                    top_speed = scaler * mean_topspeed(analyser, image_folder)
+
+                    row.extend([max_amplitude, end_amplitude, latency_value, top_speed])
+
+                writer.writerow(row)
         
                 i_fit += 1
 
